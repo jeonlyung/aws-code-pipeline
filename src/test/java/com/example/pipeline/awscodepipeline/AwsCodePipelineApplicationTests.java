217abdfd46b251aa56552abfc6c2c30e2cde6214
jeonlyung <bindongx2@naver.com>
2024-04-29 16:10:22 +0900

    first commit

---
+++ src/test/java/com/example/pipeline/awscodepipeline/AwsCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipeline.awscodepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
